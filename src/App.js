878c3eee67ed13ad0d593329cd9b838fcb5de26d
Scott Macmillan <smacmillan@greathorn.com>
2017-06-06 21:53:15 -0400

    Revamp header css and add image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import logo from './images/old-clock.svg';
 
 class App extends Component {
   render() {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <span className="header-text">Progress Clocks</span>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
