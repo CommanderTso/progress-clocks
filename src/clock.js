c4d03d7492e777ed89bee111ee41b9fa954453e3
Scott Macmillan <smacmillan@greathorn.com>
2017-06-06 22:14:39 -0400

    Start of clock component class and renderer

--- src/clock.js
+++ src/clock.js
@@ -1,2 +1,14 @@
 import React, { Component } from 'react'
 import './css/clock.css'
+
+class Clock extends Component {
+  render() {
+    return (
+      <div className="clock">
+        <img></img>
+      </div>
+    )
+  }
+}
+
+export default Clock
