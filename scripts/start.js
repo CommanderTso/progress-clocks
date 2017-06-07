acab70d1367814c1839326a4d71a27b2b8f2beb2
Scott Macmillan <smacmillan@greathorn.com>
2017-06-06 23:23:49 -0400

    Add back a semi-colon whose loss caused the build to break

--- scripts/start.js
+++ scripts/start.js
@@ -75,7 +75,7 @@ choosePort(HOST, DEFAULT_PORT)
       }
       console.log(chalk.cyan('Starting the development server...\n'))
       openBrowser(urls.localUrlForBrowser)
-    })
+    });
 
     ['SIGINT', 'SIGTERM'].forEach(function(sig) {
       process.on(sig, function() {
