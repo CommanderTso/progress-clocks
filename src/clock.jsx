c5720e1329a166ba98dda85b428509c6d4afddfa
Scott Macmillan <smacmillan@greathorn.com>
2017-06-06 23:01:27 -0400

    Basics of a clock component

--- src/clock.jsx
+++ src/clock.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './css/clock.css'
+import sixClock from './images/six-clock-empty.svg'
 
 class Clock extends Component {
   render() {
     return (
       <div className="clock">
-        
+        <img alt='clock' src={sixClock}></img>
       </div>
     )
   }
