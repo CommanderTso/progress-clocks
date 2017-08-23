a9173758709c113a93974c5cbb12e23f7c800893
Scott Macmillan <smacmillan@greathorn.com>
2017-08-22 22:21:59 -0400

    Add clocks router

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 var express = require('express')
 var app = express()
+var clocks = require('./clocks.js')
 
 var helmet = require('helmet')
 app.use(helmet())
 
-app.get("/test", function(req, res) {
-	res.send({success: true, foo: 'bar'})
-})
+app.use("/clocks", clocks)
 
 app.listen(3000, function() {
 	console.log("App listening on port 3000")
